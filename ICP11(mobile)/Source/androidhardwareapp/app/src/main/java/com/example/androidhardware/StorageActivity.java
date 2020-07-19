486fddb67714ae4fbb8cd05cbe2ecb8a8f48dad1
Sai Jaswanth Gattidi <60525536+saijaswanthgattidi@users.noreply.github.com>
2020-07-19 10:24:17 -0500

    Update StorageActivity.java

--- ICP11(mobile)/Source/androidhardwareapp/app/src/main/java/com/example/androidhardware/StorageActivity.java
+++ ICP11(mobile)/Source/androidhardwareapp/app/src/main/java/com/example/androidhardware/StorageActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
