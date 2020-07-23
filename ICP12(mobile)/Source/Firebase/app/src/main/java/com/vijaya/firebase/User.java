126b98ae604785dadad8f21efe640096b55bdb1b
saijaswanthgattidi <sgxk6@mail.umkc.edu>
2020-07-23 13:10:51 -0500

    icp12

---
+++ ICP12(mobile)/Source/Firebase/app/src/main/java/com/vijaya/firebase/User.java
@@ -0,0 +1,14 @@
+package com.vijaya.firebase;
+public class User {
+        public String name;
+        public String phone;
+        // Default constructor required for calls to
+        // DataSnapshot.getValue(User.class)
+        public User() {
+        }
+
+        public User(String name, String phone) {
+            this.name = name;
+            this.phone = phone;
+        }
+}
