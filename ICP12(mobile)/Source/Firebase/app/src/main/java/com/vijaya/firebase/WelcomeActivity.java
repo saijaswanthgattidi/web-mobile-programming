5940fd391f7554e4abb939f200e27a1f4d48e010
Sai Jaswanth Gattidi <60525536+saijaswanthgattidi@users.noreply.github.com>
2020-07-23 23:17:06 -0500

    Update WelcomeActivity.java

--- ICP12(mobile)/Source/Firebase/app/src/main/java/com/vijaya/firebase/WelcomeActivity.java
+++ ICP12(mobile)/Source/Firebase/app/src/main/java/com/vijaya/firebase/WelcomeActivity.java
@@ -1,18 +1,14 @@
 package com.vijaya.firebase;
-
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
 public class WelcomeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
     }
-
     public void redirectToSignUpPage(View v) {
         Intent redirect = new Intent(WelcomeActivity.this, SignUpActivity.class);
         startActivity(redirect);
