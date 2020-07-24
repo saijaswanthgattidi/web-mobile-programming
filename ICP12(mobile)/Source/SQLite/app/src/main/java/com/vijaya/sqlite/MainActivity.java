2bf9b04e8559330c4f842b89b2decd04774203c4
Sai Jaswanth Gattidi <60525536+saijaswanthgattidi@users.noreply.github.com>
2020-07-24 00:21:40 -0500

    Update MainActivity.java

--- ICP12(mobile)/Source/SQLite/app/src/main/java/com/vijaya/sqlite/MainActivity.java
+++ ICP12(mobile)/Source/SQLite/app/src/main/java/com/vijaya/sqlite/MainActivity.java
@@ -11,12 +11,10 @@ import com.vijaya.sqlite.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
         binding.firstActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
